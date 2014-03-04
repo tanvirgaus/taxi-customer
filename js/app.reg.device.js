a5f732ff6082ab350f63a6e45afb240301f01104
Md. Zahidul Islam <zahidulislam2008@gmail.com>
2014-03-04 12:07:48 +0600

    device id

---
+++ js/app.reg.device.js
@@ -0,0 +1,46 @@
+			//alert('Yess');
+			
+			var pushNotification;
+            
+			function callbackAPPID(data) {
+				//alert('yesssssssssssssss');
+				window.location.href = "my-vehicles.html";
+							
+			}	
+            // handle GCM notifications for Android
+			
+           
+            // handle APNS notifications for iOS
+            function onNotificationAPN(e) {
+                if (e.alert) {
+                     $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+                     navigator.notification.alert(e.alert);
+                }
+                    
+                if (e.sound) {
+                    var snd = new Media(e.sound);
+                    snd.play();
+                }
+                
+                if (e.badge) {
+                    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+                }
+            }
+
+            function onNotificationGCM(e) {
+				switch( e.event ) {
+                    case 'registered':
+					if ( e.regid.length > 0 ) {
+						currentUser = getLocalStorage("User");
+						//userId = currentUser.id;
+						params = { callback : 'callbackAPPID', controller : 'Users', action : 'appid', data : [{ deviceRegId : e.regid, userId : currentUser.id }] }; 
+						getAjaxData(params, 'callbackAPPID');
+					}
+                    break;
+                }
+            }
+            
+            function tokenHandler (result) { }
+			function successHandler (result) {  }
+            function errorHandler (error) { }
+            
